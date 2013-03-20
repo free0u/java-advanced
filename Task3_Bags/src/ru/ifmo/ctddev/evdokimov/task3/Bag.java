1706c8e32858189146a9aa47ed395840b48526d7
Anton Evdokimov <free0u@gmail.com>
2013-03-20 20:18:16 +0400

    Add Bag methods. But BagIterator still wrong

--- Task3_Bags/src/ru/ifmo/ctddev/evdokimov/task3/Bag.java
+++ Task3_Bags/src/ru/ifmo/ctddev/evdokimov/task3/Bag.java
@@ -1,16 +1,38 @@
 package ru.ifmo.ctddev.evdokimov.task3;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.AbstractCollection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
 public class Bag extends AbstractCollection<Object> {
+	private long cntElements;
+	private int modCount;
 
-	class BagIterator implements Iterator<Object> {
+	private HashMap<Object, List<Object>> data;
+	
+	public Bag() {
+		data = new HashMap<Object, List<Object>>();
+	}
+
+	private class BagIterator implements Iterator<Object> {
+		int expectedModCount;
+		Object currentValue, nextValue;
+		
+		public BagIterator() {
+			expectedModCount = modCount;
+			// TODO stub
+		}
 
 		@Override
 		public boolean hasNext() {
-			// TODO Auto-generated method stub
-			return false;
+			return nextValue != null;
 		}
 
 		@Override
@@ -21,28 +43,75 @@ public class Bag extends AbstractCollection<Object> {
 
 		@Override
 		public void remove() {
+			if (currentValue == null) {
+				throw new IllegalStateException();
+			}
+			if (expectedModCount != modCount) {
+				throw new ConcurrentModificationException();
+			}
+			// TODO delete
 			// TODO Auto-generated method stub
-			
 		}
-		
+
 	}
 	
 	@Override
 	public boolean add(Object e) {
-		// TODO realisation
-		return false;
+		List<Object> list;
+		if (data.containsKey(e)) {
+			list = data.get(e);
+		} else {
+			list = new ArrayList<>();
+		}
+		list.add(e);
+		data.put(e, list);
+		
+		cntElements++;
+		modCount++;
+		
+		return true;
+	}
+
+	@Override
+	public void clear() {
+		cntElements = 0;
+		modCount = 0;
+		data = new HashMap<Object, List<Object>>();
 	}
 	
 	@Override
-	public Iterator<Object> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean contains(Object e) {
+		return data.containsKey(e);
+	}
+	
+	@Override
+	public boolean remove(Object e) {
+		if (contains(e)) {
+			List<Object> list = data.get(e);
+			list.remove(list.size() - 1);
+			if (list.isEmpty()) {
+				data.remove(e);
+			}
+			cntElements--;
+			modCount++;
+			return true;
+		}
+		return false;
+	}
+	
+	
+	@Override
+	public BagIterator iterator() {
+		return new BagIterator();
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		if (cntElements > Integer.MAX_VALUE) {
+			return Integer.MAX_VALUE;
+		} else {
+			return (int)cntElements;
+		}
 	}
-	
+
 }
