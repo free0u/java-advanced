a000006e37fa1845dd253cf11d79b679483fc980
Anton Evdokimov <free0u@gmail.com>
2013-05-04 03:48:51 +0400

    Add implementation for Invoker

--- Task5_Invoker/src/ru/ifmo/ctddev/evdokimov/task5/Invoker.java
+++ Task5_Invoker/src/ru/ifmo/ctddev/evdokimov/task5/Invoker.java
@@ -1,11 +1,84 @@
 package ru.ifmo.ctddev.evdokimov.task5;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
 public class Invoker {
 
 	public static void main(String[] args) {
 		if (args.length < 2) {
 			System.out.println("Usage: Invoker <full-class-name> <method-name> <method-arg...>");
 			return;
+		}
+		String className = args[0];
+		String methodName = args[1];
+		
+		Object[] newArgs = Arrays.copyOfRange(args, 2, args.length);
+		
+		try {
+			Class<?> c = Class.forName(className);
+			
+			int mod = c.getModifiers();
+			
+			if (Modifier.isStatic(mod)) {
+				System.out.println(String.format("Class %s is static", className));
+			} else if (Modifier.isInterface(mod)) {
+				System.out.println(String.format("Class %s is interface", className));
+			} else if (Modifier.isAbstract(mod)) {
+				System.out.println(String.format("Class %s is abstract", className));
+			} else {
+				Object loadClass;
+				try {
+					loadClass = c.newInstance();
+				} catch (InstantiationException ignore) {
+					ignore.printStackTrace();
+					return;
+				} catch (IllegalAccessException e) {
+					System.out.println(String.format("Class %s don't have ctor", className));
+					e.printStackTrace();
+					return;
+				}
+				
+				for (Method method : c.getMethods()) {
+					if (method.getName().equals(methodName)) {
+						Class<?>[] ps = method.getParameterTypes();
+						
+						boolean correctArgs = false;
+						if (newArgs.length == ps.length) {
+							correctArgs = true;
+							for (int i = 0; i < ps.length; i++) {
+								if (!(ps[i].equals(Object.class) || 
+									ps[i].equals(String.class))) {
+									correctArgs = false;
+									break;
+								}
+							}
+						}
+						
+						if (!correctArgs) {
+							continue;
+						}
+	
+						System.out.println(method);
+						
+						try {
+							method.invoke(loadClass, newArgs);
+							System.out.println(loadClass);
+						} catch (IllegalAccessException ignore) {
+							ignore.printStackTrace();
+						} catch (IllegalArgumentException ignore) {
+							ignore.printStackTrace();
+						} catch (InvocationTargetException e) {
+							System.out.println(String.format("Method %s throws an exception", method));
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println(String.format("Class %s not found", className));
 		}
 	}
 }
