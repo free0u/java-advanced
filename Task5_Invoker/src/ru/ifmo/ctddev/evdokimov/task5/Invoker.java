fc467e4cc3d031cd8d75bf43dc8bcce4a81dccdc
Anton Evdokimov <free0u@gmail.com>
2013-05-04 01:47:30 +0400

    added project for Task5 - Invoker

---
+++ Task5_Invoker/src/ru/ifmo/ctddev/evdokimov/task5/Invoker.java
@@ -0,0 +1,12 @@
+package ru.ifmo.ctddev.evdokimov.task5;
+
+public class Invoker {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
