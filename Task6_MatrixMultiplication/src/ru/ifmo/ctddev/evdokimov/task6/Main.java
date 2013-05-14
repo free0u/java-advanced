f05d64bc809dd816ce2e1cfed248877494cb8f84
free0u <free0u@gmail.com>
2013-05-15 00:16:12 +0400

    Add realization and tests

--- Task6_MatrixMultiplication/src/ru/ifmo/ctddev/evdokimov/task6/Main.java
+++ Task6_MatrixMultiplication/src/ru/ifmo/ctddev/evdokimov/task6/Main.java
@@ -4,10 +4,39 @@ public class Main {
 
 	/**
 	 * @param args
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
+	public static void main(String[] args) throws Exception {
+		//MatrixMultiplication mm2 = new MatrixMultiplication(4, 5);
+		
+		int n = 400;
+		int m = 16;
+		
+		double[] time = new double[m];
+		final int COUNT_REPEAT = 5;
+		
+		
+		for (int i = 1; i <= m; i++) {
+			System.out.format("%d ", i);
+			
+			double sum = 0;
+			
+			for (int j = 0; j < COUNT_REPEAT; j++) {
+				long start = System.nanoTime();
+				new MatrixMultiplication(n, i);
+				long d = System.nanoTime() - start;
+				
+				sum += d;
+			}
+			
+			sum /= COUNT_REPEAT;
+			
+			
+			time[i - 1] = sum;
+		}
+		System.out.println();
+		for (int i = 0; i < m; i++) {
+			System.out.format("%f\n", time[i]);
+		}
 	}
-
 }
--- Task6_MatrixMultiplication/src/ru/ifmo/ctddev/evdokimov/task6/MatrixMultiplication.java
+++ Task6_MatrixMultiplication/src/ru/ifmo/ctddev/evdokimov/task6/MatrixMultiplication.java
@@ -1,5 +1,148 @@
 package ru.ifmo.ctddev.evdokimov.task6;
 
-public class MatrixMultiplication {
+import java.util.Arrays;
+import java.util.Random;
 
+public class MatrixMultiplication {
+	private int n;
+	private int[][] A, B, resultMatrix;
+	
+	/**
+	 * Print matrix to stdout
+	 * 
+	 * @param matrix matrix to print
+	 */
+	private void dumpMatrix(int[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				System.out.format("%d ", matrix[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	private boolean checkResult(int[][] a, int[][] b, int[][] c) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				int res = 0;
+				for (int k = 0; k < n; k++) {
+					res += a[i][k] * b[k][j];
+				}
+				
+				if (res != c[i][j]) return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	
+	/**
+	 * 
+	 * @param n size of matrix
+	 * @param m count of threads
+	 * @throws Exception 
+	 */
+	public MatrixMultiplication(int n, int m) throws Exception {
+		Random random = new Random();
+		
+		this.n = n;
+		
+		A = new int[n][n];
+		B = new int[n][n];
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				A[i][j] = random.nextInt(10);
+				B[i][j] = random.nextInt(10);
+			}
+		}
+		
+		resultMatrix = new int[n][n];
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				int res = 0;
+				for (int k = 0; k < n; k++) {
+					res += A[i][k] * B[k][j];
+				}
+				resultMatrix[i][j] = res;
+			}
+		}
+		
+
+		int n2 = n * n;
+		
+		double dblocks = ((double) n2 / m);
+		int blocks = (int) Math.ceil(dblocks);
+		if (n2 % m == 0) {
+			blocks = n2 / m;
+		}
+		
+		//int blocks = n2 / m;
+		//if (n2 % m != 0) {
+		//	blocks++;
+		//}
+		//System.out.println(blocks);
+		
+		Thread[] threads = new Thread[m];
+		
+		int[] indx = new int[n2];
+		int pos = 0;
+		int count;
+		int indTread = 0;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < blocks; j++) {
+				indx[j] = pos++;
+			}
+			
+			
+			count = blocks;
+			while (count > 0 && indx[count - 1] >= n2) {
+				count--;
+			}
+			
+			threads[indTread] = new Thread(new Worker(indx, count));
+			threads[indTread].start();
+			
+			indTread++;
+		}
+		
+		for (int i = 0; i < m; i++) {
+			try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+//		if (!checkResult(A, B, resultMatrix)) {
+//			throw new Exception("Ans wrong");
+//		}
+	}
+	
+	private class Worker implements Runnable {
+		int[] indx;
+		public Worker(int[] indx, int count) {
+			this.indx = Arrays.copyOf(indx, count);
+		}
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			for (int g = 0; g < indx.length; g++) {
+				int i = indx[g] / n;
+				int j = indx[g] % n;
+				
+				int res = 0;
+				for (int k = 0; k < n; k++) {
+					res += A[i][k] * B[k][j];
+				}
+				
+				resultMatrix[i][j] = res;
+			}
+		}
+		
+	}
 }
